8eefe8e9de4deb59ae204ef879b92b04a6fff035
ionic-project <carlped417@gmail.com>
2024-01-17 15:47:20 -0600

    User Response Final

--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -16,16 +16,52 @@ const AssetList = () => {
               <div>
                 {asset.map((a, i) => {
                   return (
-                    <div key={i}>
+                    <div
+                      style={{
+                        border: "solid",
+                        borderBlockColor: "dimgrey",
+                        padding: "20px",
+                        borderRadius: "10px",
+                        margin: "auto",
+                        marginTop: "10px",
+                        width: "50%",
+                        position: "relative"
+                      }}
+                      key={i}
+                    >
                       <div>
                         <h2>
                           {a.UserDatum.first_name} | {a.UserDatum.last_name}
                         </h2>
-                        <img src={a.imageLink} alt="here goes the images" />
+                        {/* <img src={a.imageLink} alt="here goes the images" /> */}
 
                         <p>{a.message}</p>
+                        {a.UserResponses && a.UserResponses.length > 0 ? (
+                          <div key={a.UserResponses.responses_id}>
+                            {a.UserResponses.map((r, i) => (
+                              <div
+                                style={{
+                                  backgroundColor: "dimgray",
+                                  padding: "2px",
+                                  borderRadius: "15px",
+                                  color: "white",
+                                  marginTop: "5px"
+                                }}
+                                key={i}
+                              >
+                                <p>
+                                  <strong>{r.UserDatum.username}</strong>
+                                </p>
+                                <p>{r.reply}</p>
+                                <p>{r.reactions}</p>
+                              </div>
+                            ))}
+                          </div>
+                        ) : (
+                          <>No replies yet!</>
+                        )}
                       </div>
-                      Video Screen here<div>{a.videoLink}</div>
+                      {/* Video Screen here<div>{a.videoLink}</div> */}
                       <Link to={`/${a.asset_id}/reply`}>reply</Link>
                     </div>
                   );
