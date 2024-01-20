5aaaa57fb82fa223cdb0a06388c6f0c0f9b6ffe1
ionic-project <carlped417@gmail.com>
2024-01-19 21:15:13 -0600

    Feaured Video Section

---
+++ src/Web_Resources/YouTubeLinks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const YouTubeLinks = () => {
+  const ytLinks = [
+    "https://youtu.be/Mi_QNN3aiD4?si=S_dwR0__rfbRG8Md",
+    "https://www.youtube.com/watch?v=4ZlPtapuJNs",
+    "https://www.youtube.com/watch?v=xy_px_mi0jQ",
+    "https://www.youtube.com/watch?v=8IXWSipYYXc"
+  ];
+  return ytLinks;
+};
+
+export default YouTubeLinks;
