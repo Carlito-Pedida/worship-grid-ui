7098285a0147974819b76f7a9826ec6155dd347e
ionic-project <carlped417@gmail.com>
2024-01-30 07:58:30 -0600

    Google Map Re initialization. Lots of bugs

--- src/components/LocateChurch.js
+++ src/components/LocateChurch.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 import {
   APIProvider,
   Map,
@@ -9,16 +10,21 @@ import {
 } from "@vis.gl/react-google-maps";
 
 export default function LocateChurch() {
-  const position = { lat: 61.2176, lng: -149.8997 };
+  const position = { lat: 32.615251638542006, lng: -97.42433402306592 };
   const [openPin, setOpenPin] = useState(false);
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_WG_GOOGLE_MAPS_API_KEY}>
-      <div style={{ height: "100vh", width: "100%" }}>
+      <div
+        style={{
+          height: "50vh",
+          width: "100%"
+        }}
+      >
         React google Maps
         <Map
           center={position}
-          zoom={14}
+          zoom={10}
           mapId={process.env.REACT_APP_WG_GOOGLE_MAPS_MAP_ID}
         >
           <AdvancedMarker position={position} onClick={() => setOpenPin(true)}>
@@ -35,8 +41,8 @@ export default function LocateChurch() {
               position={position}
               onCloseClick={() => setOpenPin(false)}
             >
-              <p>This is where I am at!</p>
-              <img src="/logo" height={100} />
+              <h3>This is where I am at!</h3>
+              <img src="/logo-2.png" height={100} />
             </InfoWindow>
           )}
         </Map>
