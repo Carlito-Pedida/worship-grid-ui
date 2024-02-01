be4fce06f8f0c03e1b359182e0bff005ae9d401c
ionic-project <carlped417@gmail.com>
2024-01-31 20:47:20 -0600

    RSS Page

--- src/components/Rss.js
+++ src/components/Rss.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Rss = () => {
-  return <div>Rss</div>;
+  useEffect(() => {
+    // Update the HTML title when the component mounts
+    document.title = "Worship Grid - Events";
+
+    // Optionally, you can reset the title when the component unmounts
+    //   return () => {
+    //     document.title = "Worship Grid";
+    //   };
+  }, []);
+  return (
+    <AdsBanner
+      header="THIS IS THE PAGE FOR RSS"
+      textFx="FEEDS!"
+      subHeader="Check back next time for updates"
+      buttonText2="LEARN MORE"
+    />
+  );
 };
 
 export default Rss;
