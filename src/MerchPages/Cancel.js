dcc69ea6bf0f2182caee9b1195bde2add6aa8f5a
ionic-project <carlped417@gmail.com>
2024-02-01 11:41:24 -0600

    cancel page init

--- src/MerchPages/Cancel.js
+++ src/MerchPages/Cancel.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import MerchBanner from "../props/MerchBanner";
 
 const Cancel = () => {
-  return <div>Cancel</div>;
+  return (
+    <Container>
+      <div style={{ margin: "50px", borderRadius: "10px" }}>
+        <MerchBanner
+          title2="Transaction Canceled!"
+          subTitle1="We look forward to your next transaction with us!"
+          buttonText="Go back to WG Store"
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default Cancel;
