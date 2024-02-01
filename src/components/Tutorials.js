b6f384f6fe95c392da69b91dd64a3a4113130e97
ionic-project <carlped417@gmail.com>
2024-01-31 20:52:58 -0600

    Tutorials Page Init

--- src/components/Tutorials.js
+++ src/components/Tutorials.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Tutorials = () => {
-  return <div>Tutorials</div>;
+  useEffect(() => {
+    // Update the HTML title when the component mounts
+    document.title = "Worship Grid - Tutorials";
+
+    // Optionally, you can reset the title when the component unmounts
+    //   return () => {
+    //     document.title = "Worship Grid";
+    //   };
+  }, []);
+  return (
+    <AdsBanner
+      header="SOMETHING AWESOME COMING"
+      textFx="SOON!"
+      subHeader="Check back next time for updates"
+      buttonText2="LEARN MORE"
+    />
+  );
 };
 
 export default Tutorials;
