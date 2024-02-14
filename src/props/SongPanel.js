ead5ebd0e5574ae52284487865aa1d2957f97410
ionic-project <carlped417@gmail.com>
2024-02-14 11:57:28 -0600

    setlist page

---
+++ src/props/SongPanel.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "../styles/UserSetlist.css";
+
+const SongPanel = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="tab-case">
+      <div className="tab-header">
+        {tabs.map((tab, index) => (
+          <div>
+            <div
+              key={index}
+              className={`tab ${activeTab === index ? "active" : ""}`}
+              onClick={() => handleClick(index)}
+            >
+              {tab.title}
+              <div className="key mx-2">{tab.key}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="tab-content">
+        <iframe className="songIframe" src={tabs[activeTab].content} />
+      </div>
+    </div>
+  );
+};
+
+export default SongPanel;
