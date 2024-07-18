1e10e088233bc9151a2d2f056439637fc98cd91b
ionic-project <carlped417@gmail.com>
2024-07-17 20:05:21 -0500

    Prod update version 5

--- src/components/Rss.js
+++ src/components/Rss.js
@@ -51,11 +51,10 @@ const Rss = () => {
         className="rss-main col-12 col-md-12 col-lg-9"
         style={{
           background: `url('https://images.unsplash.com/photo-1643148636641-fc498af944c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
-          backgroundSize: "100%",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          position: "relative" // Necessary for absolute positioning of children
+          position: "relative"
         }}
       >
         <div className="rss-content">
